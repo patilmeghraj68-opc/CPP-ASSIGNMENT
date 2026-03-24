e5420317380d7b5ef490f5a9d90f4f2f1bbe43aa
patilmeghraj68-opc <patilmeghraj68@gmail.com>
2026-03-24 16:22:25 +0530

    Add files via upload

---
+++ CPP-ASSIGMENT-main/7swap.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+class Swap {
+    public :
+    int a,b;
+
+
+    void getinfo(){
+        cout<<"Enter the number a and b ";
+        cin>>a>>b;
+    }
+
+    void swapnumber(){
+        int temp=a;
+        a=b;
+        b=temp;
+    }
+
+    void showinfo(){
+        cout<<"a = "<<a <<endl <<"b = "<<b <<endl;
+    }
+
+};
+
+int main(){
+Swap prasad;
+prasad.getinfo();
+prasad.swapnumber();
+prasad.showinfo();
+
+}
+
+
+
